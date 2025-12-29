281dc71e7d99d718ea3615d24618bbd46e5910da
Ronald A. Richardson <me@ron.dev>
2025-12-29 14:32:43 +0800

    feat: Added missing `started_at` order prop

--- addon/models/order.js
+++ addon/models/order.js
@@ -95,6 +95,7 @@ export default class OrderModel extends Model {
     /** @dates */
     @attr('date') scheduled_at;
     @attr('date') dispatched_at;
+    @attr('date') started_at;
     @attr('date') deleted_at;
     @attr('date') created_at;
     @attr('date') updated_at;
